fcbc8f7243d72e46600a8ca5ad1335b5bd912017
rivaldani <muhammadrivaldani@student.uir.ac.id>
2024-06-25 17:32:33 +0700

    baru

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/nama-aplikasi/' : '/',
+};
 export default defineConfig({
   plugins: [
     vue({
