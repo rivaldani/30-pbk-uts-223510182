5b1d83c2f34b73552716a6036521f7718b68e380
rivaldani <muhammadrivaldani@student.uir.ac.id>
2024-06-25 17:25:06 +0700

    baru

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/index';
+import './assets/styles.css';
 import { createPinia } from 'pinia';
 import { Quasar } from 'quasar'; // Mengimpor Quasar dari paket yang benar
 
